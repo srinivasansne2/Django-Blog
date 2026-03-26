dff1cc2e43ea8756968756476dfa71af1669a15f
srinivasansne2 <srinivasansne2@gmail.com>
2026-03-26 19:42:32 +0530

    Updated project for deployment

--- myapp/settings.py
+++ myapp/settings.py
@@ -88,16 +88,14 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 #     }
 # }
 
-import dj_database_url
 import os
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
