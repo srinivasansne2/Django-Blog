0f8f5cde9a2633713e3827a33ba49e31c5cd0c6a
srinivasansne2 <srinivasansne2@gmail.com>
2026-04-03 00:40:06 +0530

    Fix template path for deployment

--- myapp/settings.py
+++ myapp/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'myapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
